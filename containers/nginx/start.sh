1020fc410958561acde4b382da3b284079d8d345
Ethan Welborn <ethanwelborn@protonmail.com>
2024-05-29 00:32:24 -0500

    Testing GoAccess Log Interface
    Added some basic duct-tape code to get GoAccess working - expect several commits to patch bugs in the near future. A lot of code was based on this tutorial series: https://bytes.fyi/real-time-goaccess-reports-with-nginx/

--- containers/nginx/start.sh
+++ containers/nginx/start.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 # Replace all environment variables in default.conf
-envsubst '$FLASK_SERVER_ADDR,$DOMAIN_NAME' < /tmp/default.conf > /etc/nginx/conf.d/default.conf
+envsubst '$FLASK_SERVER_ADDR,$GOACCESS_SERVER_ADDR,$DOMAIN_NAME,$GOACCESS_DOMAIN_NAME,$GOACCESS_ADMIN_IP_ADDRESS' < /tmp/default.conf > /etc/nginx/conf.d/default.conf
 
 #if [ ! -f /etc/letsencrypt/live/$DOMAIN_NAME/fullchain.pem ]; then \
 #    certbot certonly --standalone --non-interactive --agree-tos --email $EMAIL --preferred-challenges http --domains $DOMAIN_NAME,www.$DOMAIN_NAME\
